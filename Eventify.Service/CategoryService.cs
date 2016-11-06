78f3a44a3ff8279cca030cdf7ae81921f2720409
IbrahimSassi <sassiibrahim12@gmail.com>
2016-11-06 12:16:26 +0100

    First Test for adding

---
+++ Eventify.Service/CategoryService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eventify.Data.Infrastructure;
+using Eventify.Data.Models;
+using Service.Pattern;
+
+namespace Eventify.Service
+{
+    public class CategoryService : MyServiceGeneric<Category>,ICategoryService
+    {
+
+        private static IDataBaseFactory dbfac = new DataBaseFactory();
+
+        private static IUnitOfWork itw = new UnitOfWork(dbfac);
+
+        public CategoryService() : base(itw)
+        {
+
+        }
+
+    }
+}
