a278c0ec7c17f4af793a5af86abd553690929d7e
MrFirases <mohamedfiras.ouertani@esprit.tn>
2016-11-12 18:49:57 +0100

    Adding user service.

--- Eventify.Service/IUserService.cs
+++ Eventify.Service/IUserService.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Eventify.Service
 {
-    public interface IUserService : IMyServiceGeneric<User>
+    interface IUserService : IMyServiceGeneric<User>
     {
     }
 }
