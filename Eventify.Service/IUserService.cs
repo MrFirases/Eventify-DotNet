2bd03e70539224d9ee7febdd62081bdcc5be7b98
MrFirases <mohamedfiras.ouertani@esprit.tn>
2016-11-12 18:40:16 +0100

    Adding a very basic "Get User" from database. (See results localhost:port/User)

---
+++ Eventify.Service/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Eventify.Data.Models;
+using Service.Pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventify.Service
+{
+    public interface IUserService : IMyServiceGeneric<User>
+    {
+    }
+}
---
+++ Eventify.Web/Controllers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using Eventify.Data.Models;
+using Eventify.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eventify.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            IEnumerable<User> user;
+            IUserService userService = new UserService();
+            user = userService.GetMany();
+            userService.commit();
+
+            return View(user);
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
