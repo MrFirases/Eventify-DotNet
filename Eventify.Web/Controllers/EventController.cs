122f97432d3e5f2a4bbaf78f5323397e6a9f1dd3
HakimMliki <ovtestovtest@gmail.com>
2016-11-12 18:44:38 +0100

    Cleaning Layout and Menu | Adding Eventify View Test

--- Eventify.Web/Controllers/EventController.cs
+++ Eventify.Web/Controllers/EventController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Eventify.Service;
 
 namespace Eventify.Web.Controllers
 {
+    //[Authorize]
     public class EventController : Controller
     {
+        private IEventService eventService = null;
+
+        public EventController()
+        {
+            eventService = new EventService();
+        }
         // GET: Event
         public ActionResult Index()
         {
-            return View();
+            var events = eventService.GetMany();
+            return View(events);
         }
 
         // GET: Event/Details/5
