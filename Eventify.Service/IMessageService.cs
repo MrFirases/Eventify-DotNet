e3647c7ecc837ee6ce2ba1f933e42413f265285c
IbrahimSassi <sassiibrahim12@gmail.com>
2016-11-12 18:30:19 +0100

    Starting Reservation Service

---
+++ Eventify.Service/IMessageService.cs
@@ -0,0 +1,17 @@
+﻿using Eventify.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service.Pattern;
+
+namespace Eventify.Service
+{
+    public interface IMessageService : IMyServiceGeneric<Message>
+    {
+        int countClaimNotResponded();
+        int countClaim();
+        IEnumerable<Message> getClaimsNotResponded();
+    }
+}
