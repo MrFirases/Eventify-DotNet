14fbed0f6ab7a5a94b4b77cebc0a028560448c88
IbrahimSassi <ibrahim.sassi@esprit.tn>
2016-11-16 14:27:58 +0100

    Testing Consuming Rest WebApi

--- Eventify.Web/Startup.cs
+++ Eventify.Web/Startup.cs
@@ -1,14 +1,30 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin;
+using Microsoft.Owin.Logging;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(Eventify.Web.Startup))]
+
 namespace Eventify.Web
 {
     public partial class Startup
+
     {
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
         }
+
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    //Add Cors support to the service
+        //    services.AddCors(options => options.AddPolicy("AllowAllOrigins", builder => builder.AllowAnyOrigin()));
+
+        //}
+
+
+
     }
 }
