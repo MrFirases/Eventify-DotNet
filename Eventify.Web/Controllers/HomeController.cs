3cd5ce8148894ede4cad4749dba1d331d391b220
IbrahimSassi <ibrahim.sassi@esprit.tn>
2016-11-19 19:44:22 +0100

    Adding Message For Index + Email Is Working #Email Is Mine

--- Eventify.Web/Controllers/HomeController.cs
+++ Eventify.Web/Controllers/HomeController.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Eventify.Data.Models;
 using Eventify.Service;
+using Microsoft.Ajax.Utilities;
 
 namespace Eventify.Web.Controllers
 {
     public class HomeController : Controller
     {
          IEventService eventService = null;
+        private IMessageService messageService = null;
 
         public HomeController()
         {
             eventService = new EventService();
+            messageService = new MessageService();
         }
         public ActionResult Index()
         {
+            IEnumerable<Message> messages =
+                messageService.GetMany(message => message.sended == true && message.claim == true).DistinctBy(message => message.user_id).ToList();
+
+            Session["MessageReceived"] = messages;
+            Session["NbNewMessage"] = messages.Count();
             return View();
         }
 
